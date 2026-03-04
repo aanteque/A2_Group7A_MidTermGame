cb9cc3725681978d92bf12a3db0d01ce406818d0
aanteque <aanteque@uwaterloo.ca>
2026-03-04 12:05:58 -0500

    BaseFilesSetup

---
+++ Player.js
@@ -0,0 +1,54 @@
+class Player {
+  constructor(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.r = 10;
+
+    // wobble visuals
+    this.t = 0;
+    this.s = speed ?? 3;
+    this.wobble = 7;
+    this.points = 48;
+    this.wobbleFreq = 0.9;
+
+    // colour
+    this.rC = 255;
+    this.gC = 255;
+    this.bC = 255;
+  }
+
+
+
+  updateInput() {
+    const dx =
+      (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) -
+      (keyIsDown(LEFT_ARROW) || keyIsDown(65));
+
+    const dy =
+      (keyIsDown(DOWN_ARROW) || keyIsDown(83)) -
+      (keyIsDown(UP_ARROW) || keyIsDown(87));
+
+    const len = max(1, abs(dx) + abs(dy));
+    this.x += (dx / len) * this.s;
+    this.y += (dy / len) * this.s;
+  }
+
+
+  draw() {
+    this.t += 0.02;
+    fill(player.rC, player.gC, player.bC);
+    noStroke();
+    beginShape();
+    for (let i = 0; i < this.points; i++) {
+      const a = (i / this.points) * TAU;
+      const n = noise(
+        cos(a) * this.wobbleFreq + 100,
+        sin(a) * this.wobbleFreq + 100,
+        this.t,
+      );
+      const rr = this.r + map(n, 0, 1, -this.wobble, this.wobble);
+      vertex(this.x + cos(a) * rr, this.y + sin(a) * rr);
+    }
+    endShape(CLOSE);
+  }
+}
---
+++ README.md
@@ -0,0 +1,56 @@
+## Project Title
+
+GBDA302 Side Quest 5 Sad Lonely Stressed Blob
+
+---
+
+## Authors
+
+Alejandra Antequera: 21073624
+
+---
+
+## Description
+
+This is a game about being a small little stressed, sad, and lonely blob.
+
+---
+
+## INSTRUCTIONS
+
+- WASD or arrow keys to move
+- R to reset position
+
+---
+
+## Iteration Notes
+
+Post-Playtest: Three changes made based on playtesting
+- Changed Colours
+- Changed Blob Appearance
+- Added an stress ball thingies
+
+Post-Showcase: Two planned improvements
+- Perhaps add damage
+- Perchance add shaky screen near enemy.
+
+---
+
+## Learning Goals
+
+Learning Goals:
+
+- Evoke emotions through visuals
+
+---
+
+## Assets
+
+ALL ASSETS MADE BY ME IN FIGMA
+
+---
+
+## GenAI
+
+NO AI
+---
